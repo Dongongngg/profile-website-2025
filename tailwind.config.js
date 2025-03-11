0e21227877b95ab7f59dafde2a0a93c224147eac
dongongngg <jingfu.dong0320@gmail.com>
2025-03-11 12:31:39 +1100

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ['./index.html', './styles/**/*.css'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'background': '#1e1e1e',
+        'text': '#ffffff',
+        'description': '#cccccc',
+        'linkedin': '#0077b5',
+      },
+      fontFamily: {
+        'ubuntu': ['Ubuntu', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
